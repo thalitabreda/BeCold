359d7a12350ad5fdf1b529e9cdfa972212cc21c8
oKaiquiPetty <kaiqui.petty@sptech.school>
2023-11-18 09:18:37 -0300

    resolvendo incidente

---
+++ siteBecold (1)/API/main.js
@@ -0,0 +1,100 @@
+const serialport = require('serialport');
+const express = require('express');
+const mysql = require('mysql2');
+
+const SERIAL_BAUD_RATE = 9600;
+const SERVIDOR_PORTA = 3000;
+
+const serial = async (
+    valoresDht11Umidade,
+    valoresDht11Temperatura
+) => {
+    const poolBancoDados = mysql.createPool(
+        {
+            host: 'localhost',
+            port: 3306,
+            user: 'root',
+            password: 'admin',
+            database: 'Becold'
+        }
+    ).promise();
+
+    const portas = await serialport.SerialPort.list();
+    const portaArduino = portas.find((porta) => porta.vendorId == 2341 && porta.productId == 43);
+    if (!portaArduino) {
+        throw new Error('O arduino não foi encontrado em nenhuma porta serial');
+    }
+    const arduino = new serialport.SerialPort(
+        {
+            path: portaArduino.path,
+            baudRate: SERIAL_BAUD_RATE
+        }
+    );
+    arduino.on('open', () => {
+        console.log(`A leitura do arduino foi iniciada na porta ${portaArduino.path} utilizando Baud Rate de ${SERIAL_BAUD_RATE}`);
+    });
+    arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
+        const valores = data.split(';');
+        const dht11Umidade = parseFloat(valores[0]);
+        const dht11Temperatura = parseFloat(valores[1]);
+
+        valoresDht11Umidade.push(dht11Umidade);
+        valoresDht11Temperatura.push(dht11Temperatura);
+
+        var idSensor = parseInt(1+Math.random() * 9);
+
+        if (true) {
+            await poolBancoDados.execute(
+                'INSERT INTO Registros (Temperatura, Umidade, fkSensor) VALUES (?, ?, ?)',
+                [dht11Temperatura, dht11Umidade, idSensor]
+            );
+        }
+
+    });
+    arduino.on('error', (mensagem) => {
+        console.error(`Erro no arduino (Mensagem: ${mensagem}`)
+    });
+}
+
+const servidor = (
+    valoresDht11Umidade,
+    valoresDht11Temperatura
+) => {
+    const app = express();
+    app.use((request, response, next) => {
+        response.header('Access-Control-Allow-Origin', '*');
+        response.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+        next();
+    });
+    app.listen(SERVIDOR_PORTA, () => {
+        console.log(`API executada com sucesso na porta ${SERVIDOR_PORTA}`);
+    });
+    app.get('/sensores/dht11/umidade', (_, response) => {
+        return response.json(valoresDht11Umidade);
+    });
+    app.get('/sensores/dht11/temperatura', (_, response) => {
+        return response.json(valoresDht11Temperatura);
+    });
+    app.get('/sensores/luminosidade', (_, response) => {
+        return response.json(valoresLuminosidade);
+    });
+    app.get('/sensores/lm35/temperatura', (_, response) => {
+        return response.json(valoresLm35Temperatura);
+    });
+    app.get('/sensores/chave', (_, response) => {
+        return response.json(valoresChave);
+    });
+}
+
+(async () => {
+    const valoresDht11Umidade = [];
+    const valoresDht11Temperatura = [];
+    await serial(
+        valoresDht11Umidade,
+        valoresDht11Temperatura
+    );
+    servidor(
+        valoresDht11Umidade,
+        valoresDht11Temperatura
+    );
+})();
